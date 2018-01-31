23682ce4c6580a9694bbf1cd9940a61d10c34f56
Alexey Filippov <afilippov@acm.org>
2018-01-31 01:46:28 +0000

    Formatting as specified in the task

--- src/main/scala/uk/filippov/addressbook/Main.scala
+++ src/main/scala/uk/filippov/addressbook/Main.scala
@@ -4,15 +4,15 @@ package uk.filippov.addressbook
 object Main extends App {
   val people = AddressBook.loadBook(io.Source.fromFile(args.head, "UTF-8").mkString)
 
-  print("1. ")
+  print("  1. ")
   println(AddressBook.countMales(people))
 
-  print("2. ")
+  print("  2. ")
   println(AddressBook.oldest(people).name)
 
   val Some(bill) = people.find(_.name.startsWith("Bill "))
   val Some(paul) = people.find(_.name.startsWith("Paul "))
 
-  print("3. ")
+  print("  3. ")
   println(AddressBook.ageDifference(bill, paul))
 }
