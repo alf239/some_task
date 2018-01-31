73a96d6dc6dc747c1de47ac131be5169663696fa
Alexey Filippov <afilippov@acm.org>
2018-01-31 01:42:10 +0000

    The actual runner

---
+++ src/main/scala/uk/filippov/addressbook/Main.scala
@@ -0,0 +1,18 @@
+package uk.filippov.addressbook
+
+// We read the address book from the file specified as the parameter for our app
+object Main extends App {
+  val people = AddressBook.loadBook(io.Source.fromFile(args.head, "UTF-8").mkString)
+
+  print("1. ")
+  println(AddressBook.countMales(people))
+
+  print("2. ")
+  println(AddressBook.oldest(people))
+
+  val Some(bill) = people.find(_.name.startsWith("Bill "))
+  val Some(paul) = people.find(_.name.startsWith("Paul "))
+
+  print("3. ")
+  println(AddressBook.ageDifference(bill, paul))
+}
